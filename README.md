4589563e6fea8c41ef7025d242385914480963bd
Josh M <josh@muir.xyz>
2021-01-30 01:28:01 +0000

    v1.0.0: Release

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# roblox-api-webhook
-Regularly checks a discourse post for new content, and posts that content to a discord channel.
+# Api-Notify
+Notifies on changes to the Roblox web api - when they are posted on the official dev forum post.
+Sends notifications via. a Discord message. This module makes use of a bot as opposed to a webhook as this allows it to crosspost messages too.
+
+# Usage
+- Clone this repo: `git clone https://github.com/polaris-rbx/roblox-api/webhook`
+- navigate to the folder
+- Install dependencies `npm install`
+- Set up config file. It should be called `config.json` and be within the same directory as `index.js`.
+
+## Config structure
+```json
+{
+  "url": "",
+  "interval": 5,
+  "channel": "",
+  "message": "",
+  "initialPost": 23,
+  "crosspost": true,
+  "token": ""
+}
+```
+## Config values
+| Key | Type | Description |
+| --- | ---  | ------------| 
+| url | String | The JSON endpoint URL. Usually the URL of the post, with `.json` appended to the end. |
+| interval | Number | The time between GET requests to check for new content. Lower = faster notifications, at the cost of more http requests. Do not set this below 0.5.|
+| channel | String(Snowflake) | The channel id of the channel  you want the messages to go in |
+| message | String |The message to prepend to the message. This is added before the URL of the latest post. |
+| initialPost | number | The first post you want to consider. If monitoring a huge post, you will need to get the post number of the latest post from the JSON endpoint. |
+| crosspost | boolean | Whether or not the bot should cross-post to following servers. Set to false if not using an announcement channel.
+| token | String | The discord bot token. The bot must have Send messages and Manage messages within the channel in question. |
+
+### Support
+Open an issue or contact me directly:
+
+Discord: Neztore#6998
+
+[Polaris discord](https://discord.gg/QevWabU)
---
+++ index.js
@@ -0,0 +1,100 @@
+const fetch = require("node-fetch");
+const { interval: intervalSeconds, url, token, channel, crosspost, initialPost, message } = require("./config.json");
+const { join } = require("path");
+const { promises: {readFile, writeFile } } = require("fs");
+const lastNumLoc = join(__dirname, "lastNum");
+const Eris = require("eris");
+
+const bot = new Eris(`Bot ${token}`, {
+    restMode: true
+})
+
+
+let lastNum = initialPost || 0;
+
+// Writes lastDate to file
+async function persistLatest () {
+    try {
+        await writeFile(lastNumLoc, `${lastNum}`)
+    } catch (e) {
+        throw new Error(`Failed to write to ${lastNumLoc}: ${e}`);
+    }
+}
+
+
+/**
+ * Gets any new posts that have been made.
+ */
+async function getNew () {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.errors) {
+        console.log(data.errors);
+        throw new Error(data.errors[0]);
+    }
+
+        const toReturn = {
+            title: data.title,
+            count: data.posts_count,
+            newPosts: []
+        }
+        for (const post of data.post_stream.posts) {
+            if (post.post_number > lastNum) {
+                lastNum = post.post_number;
+                const base = url.split("/t/")[0]
+                const avatarUrl = `${base}/${post.avatar_template.replace("{size}", "100")}`
+                toReturn.newPosts.push({
+                    content: post.cooked,
+                    username: post.username,
+                    avatarUrl,
+                    url: `${url.split(".json")[0]}/${post.post_number}`
+                })
+            }
+        }
+        persistLatest().catch(console.error);
+        return toReturn;
+
+}
+
+async function notifyPost (name, post) {
+    const m = await bot.createMessage(channel, { content: `${message}\nBy: ${post.username}.\n\n${post.url}` })
+    if (m && m.id && crosspost) {
+        await bot.crosspostMessage(channel, m.id);
+    }
+}
+
+
+async function run() {
+    const latest = await getNew();
+    if (latest.newPosts.length !== 0) {
+        const p = [];
+        for (let post of latest.newPosts) {
+            p.push(notifyPost(latest.title, post))
+        }
+        await Promise.all(p);
+    }
+}
+
+async function main () {
+    // Read initial num from file
+    try {
+        const res = await readFile(lastNumLoc)
+        lastNum = parseInt(res, 10);
+
+        if (isNaN(lastNum)) {
+            console.warn("Value from file was NaN!")
+            lastNum = initialPost || 0
+        }
+    } catch (e) {
+        if (e.code !== "ENOENT") {
+            throw new Error(`Failed to read ${lastNumLoc}: ${e}`)
+        }
+    }
+
+    setInterval(run, intervalSeconds * 60000)
+    return run();
+}
+
+
+main().catch(console.error)
